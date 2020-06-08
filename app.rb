7b6d28902e458c8468cb8eb9f606bcc815a01c58
Liam Tinney <ltinney@powerhrg.com>
2020-06-08 19:44:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  post "/checkout" do
+    params[:item]
+    session[:item] = params[:item]
+  end
 end
